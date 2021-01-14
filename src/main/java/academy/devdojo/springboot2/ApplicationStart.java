f6a0ead48b886d0808ca62fbebfd0a9154a6399c
Douglas Ferreira <douglasmfe@gmail.com>
2021-01-14 16:10:16 -0300

    first commit springboot2-essentials

---
+++ src/main/java/academy/devdojo/springboot2/ApplicationStart.java
@@ -0,0 +1,16 @@
+package academy.devdojo.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@SpringBootApplication
+//@EnableAutoConfiguration
+//@ComponentScan
+//@Configuration
+public class ApplicationStart {
+    public static void main(String[] args){
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
